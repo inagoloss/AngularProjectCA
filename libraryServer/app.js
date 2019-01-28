6636516c5b3aba4b9f977581f96bbdf59cf73ada
Mujo Ramic <32583467+MrNoOne007@users.noreply.github.com>
2019-01-28 13:10:05 +0100

    Upload - server side
    Upload component, with server side
    Node js server at port 3000.

--- libraryServer/app.js
+++ libraryServer/app.js
@@ -4,20 +4,68 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
+const fs = require('fs');
+const multer = require('multer');
+const router = express.Router();
+
+
 
 const { getBooks, addBook, deleteBook, getOneBook, editBook} = require('./routes/books');
 const port = 2000;
+const DIR = './uploads';
 
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+ 
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, DIR);
+      },
+      filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + '.' + path.extname(file.originalname));
+    }
+});
+let upload = multer({storage: storage});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+ 
+app.get('/api', function (req, res){
+  res.end('file catcher example');
+});
+ 
+app.post('/api/uploads',upload.any('file'),function (req, res) {
+    if (!req.file) {
+        console.log("No file received");
+        return res.send({
+          success: false
+        });
+    
+      } else {
+        console.log('file received');
+        return res.send({
+          success: true
+        })
+      }
+});
+
+
+//create connection to database
+//the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
+    host: '',
+    user: 'mujo',
     password: '',
     database: 'librarydb'
 });
 
-// connect to database
+//connect to database
 db.connect((err) => {
     if (err) {
         throw err;
@@ -45,3 +93,8 @@ app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
 
+const PORT = process.env.PORT || 3000;
+ 
+app.listen(PORT, function () {
+  console.log('Node.js server is running on port ' + PORT);
+});
